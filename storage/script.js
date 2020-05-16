5b7adf6425de52aabd3a540176be855164b8704d
AlexeyAleks <AlexeyAleks@mail.ru>
2020-05-16 20:31:55 +0300

    solution task 02

--- storage/script.js
+++ storage/script.js
@@ -1,3 +1,20 @@
 function submit() {
+    'use strict';
+        
+    (async () => {
+        let url = 'https://reqres.in/api/users?page=2';
+        let response = await fetch(url);
+        let user = await response.json();
+
+        let length = user.data.length;
+        for (let i = 0; i < length; i++) {
+            let fullName = user.data[i].first_name + " " + user.data[i].last_name;
+            insert.insertAdjacentHTML('beforeend', '<br>');
+            insert.insertAdjacentHTML('beforeend',  fullName);
+        };
+    })
+    ()
+
     
+
 }
