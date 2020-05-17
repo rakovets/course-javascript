f77516e927e8212446c48b05f3385a302322c20e
AlexeyAleks <AlexeyAleks@mail.ru>
2020-05-17 09:34:15 +0300

    solution tasks 01-04

--- storage/script.js
+++ storage/script.js
@@ -1,22 +1,5 @@
 function submit() {
     'use strict';
-        
-    // (async () => {
-    //     let url = 'https://reqres.in/api/users?page=2';
-    //     let response = await fetch(url);
-    //     if (response.status != 200) {
-    //         insert.insertAdjacentHTML('beforeend', 'ERROR - data not received');
-    //     };
-    //     let user = await response.json();
-
-    //     let length = user.data.length;
-    //     for (let i = 0; i < length; i++) {
-    //         let fullName = user.data[i].first_name + " " + user.data[i].last_name;
-    //         insert.insertAdjacentHTML('beforeend', '<br>');
-    //         insert.insertAdjacentHTML('beforeend',  fullName);
-    //     };
-    // })
-    // ()
     
     (async () => {
         let url = 'https://reqres.in/api/users?page=2';
@@ -27,6 +10,14 @@ function submit() {
             for (let i = 0; i < length; i++) {
                 let fullName = user.data[i].first_name + " " + user.data[i].last_name;
                 insert.insertAdjacentHTML('beforeend', '<br>' + fullName);
+
+                localStorage.setItem(`firstNameUser${i}`, user.data[i].first_name);
+                localStorage.setItem(`lastNameUser${i}`, user.data[i].last_name);
+                
+                // let userNum = localStorage.getItem(`firstNameUser${i}`) + " " 
+                //     + localStorage.getItem(`lastNameUser${i}`);
+                // alert(userNum); 
+
             }; 
         } catch (e) {
             insert.insertAdjacentHTML('beforeend', 'Data not received' + '<br>' + e.stack);
