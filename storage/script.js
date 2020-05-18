45479493ae0461a11e030f068fdc697c96155a25
AlexeyAleks <AlexeyAleks@mail.ru>
2020-05-18 21:36:19 +0300

    solution all tasks 01-06 (open: //localStorage.clear();)

--- storage/script.js
+++ storage/script.js
@@ -1,17 +1,17 @@
 function submit() {
     'use strict';
-    // localStorage.clear();
+    localStorage.clear();
     let dataLength  = localStorage.length;
     if (dataLength != 0) {
         alert("Loading from localStorage");
         for (let i = 0; i < dataLength/3; i++) {
             insertText.insertAdjacentHTML('beforebegin', `<input type="radio" name="inset" id="tab_${i}" checked>`
-                + `<label for="tab_${i}">${i + 1}. ` + localStorage.getItem(`lastNameUser${i}`) + `</label></div>`
+                + `<label for="tab_${i}">${i + 1}. ` + localStorage.getItem(`lastNameUser${i}`) + `</label>`
             );
             insertText.insertAdjacentHTML('afterend', `<div id="txt_${i}"><p><img src="` 
                 + localStorage.getItem(`avatarUser${i}`) + `" alt="photo.jpg">` 
                 + `<p><big><b>First Name: </b> <i>` + localStorage.getItem(`firstNameUser${i}`) + `</i></p>` 
-                + `<b>Last Name: </b> <i>` + localStorage.getItem(`lastNameUser${i}`) + `</i></big></p>`
+                + `<b>Last Name: </b> <i>` + localStorage.getItem(`lastNameUser${i}`) + `</i></big></p></div>`
             );
         }
     } else {
@@ -24,11 +24,13 @@ function submit() {
                 let length = user.data.length;
                 for (let i = 0; i < length; i++) {
                     insertText.insertAdjacentHTML('beforebegin', `<input type="radio" name="inset" id="tab_${i}" checked>`
-                        + `<label for="tab_${i}">${i + 1}. ` + user.data[i].last_name + `</label></div>`);
+                        + `<label for="tab_${i}">${i + 1}. ` + user.data[i].last_name + `</label>`
+                    );
                     insertText.insertAdjacentHTML('afterend', `<div id="txt_${i}"><p><img src="` 
                         + user.data[i].avatar + `" alt="photo.jpg">` 
                         + `<p><big><b>First Name: </b> <i>` + user.data[i].first_name + `</i></p>` 
-                        + `<b>Last Name: </b> <i>` + user.data[i].last_name + `</i></big></p>`);
+                        + `<b>Last Name: </b> <i>` + user.data[i].last_name + `</i></big></p></div>`
+                    );
                     localStorage.setItem(`firstNameUser${i}`, user.data[i].first_name);
                     localStorage.setItem(`lastNameUser${i}`, user.data[i].last_name);
                     localStorage.setItem(`avatarUser${i}`, user.data[i].avatar);
