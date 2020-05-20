159b53fa3ad44225a96b24d89dd230737ba23410
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-05-20 10:13:04 +0300

    done task 2

--- storage/js/main.js
+++ storage/js/main.js
@@ -1,1 +1,14 @@
+const result = document.querySelector('#result');
 
+function buttonAction() {
+    fetch('https://reqres.in/api/users?page=2')
+        .then(res => res.json())
+        .then(users => {
+            users.data.forEach(user => {
+                let dataAboutUser = user.first_name + " " + user.last_name;
+                result.insertAdjacentHTML('beforeend', dataAboutUser);
+                result.insertAdjacentHTML('beforeend', '<br />');
+            });
+        })
+        .catch(err => console.log(`Error: ${err}`));
+}
