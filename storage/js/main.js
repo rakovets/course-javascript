2e7e70f0b4e4ded12346391a34ad94932380d46f
Sergey Trutsin <sergey.trutsin@gmail.com>
2020-05-22 15:58:16 +0300

    done task 5

--- storage/js/main.js
+++ storage/js/main.js
@@ -1,16 +1,17 @@
 const result = document.querySelector('#result');
+const nav = document.querySelector('#nav');
+const bodyOfTabs = document.querySelector('#bodyOfTabs');
 let key = 0;
 
 function buttonAction() {
+    removeOldElements();
     fetch('https://reqres.in/api/users?page=2')
         .then(res => res.json())
         .then(users => {
             users.data.forEach(user => {
                 writeInStorage(user.first_name);
                 writeInStorage(user.last_name);
-                let dataAboutUser = user.first_name + " " + user.last_name;
-                result.insertAdjacentHTML('beforeend', dataAboutUser);
-                result.insertAdjacentHTML('beforeend', '<br />');
+                createNewTab(user.first_name, user.last_name);
             });
         })
         .catch(err => {
@@ -22,4 +23,30 @@ function buttonAction() {
 function writeInStorage(value) {
     localStorage.setItem(key, value);
     key++;
+}
+
+function createNewTab(firstName, lastName) {
+    let a = document.createElement('a');
+    a.className = 'tabs__item';
+    a.innerHTML = lastName;
+    a.setAttribute('href', `#tab${key}`);
+    nav.prepend(a);
+
+    let div = document.createElement('div');
+    div.className = 'tabs__block';
+    div.innerHTML = firstName + ' ' + lastName;
+    div.setAttribute('id', `tab${key}`);
+    bodyOfTabs.prepend(div);
+}
+
+function removeOldElements() {
+    let aElements = document.querySelectorAll('#nav > a');
+    aElements.forEach(element => {
+        element.remove();
+    });
+
+    let divElements = document.querySelectorAll('#bodyOfTabs > div');
+    divElements.forEach(element => {
+        element.remove();
+    });
 }
